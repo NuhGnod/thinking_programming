6bafcb8b3c8f16015a027121f95927ae2158200f
donghun <zhfptm12@naver.com>
2020-11-08 23:59:44 +0900

    cnu
    for each

--- src/F/Main.java
+++ src/F/Main.java
@@ -8,11 +8,12 @@ public class Main {
 	static int playerNum, limit, count, curLevel, next;
 	static PlayerInfo player[];
 	static LinkedList<PlayerInfo> list;
-	static ArrayList room;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		String firstInput[] = br.readLine().split(" ");
 		playerNum = Integer.parseInt(firstInput[0]);
 		limit = Integer.parseInt(firstInput[1]);
@@ -20,55 +21,50 @@ public class Main {
 		curLevel = 1;
 		next = 0;
 		player = new PlayerInfo[playerNum];
-		list = new LinkedList<>();
-		room = new ArrayList<>();
+
+		ArrayList<LinkedList<PlayerInfo>> rooms = new ArrayList();
+
 		for (int i = 0; i < playerNum; i++) {
 			String secInput[] = br.readLine().split(" ");
+			int level = Integer.parseInt(secInput[0]);
+			String id = secInput[1];
+			boolean isFind = false;
+			for (LinkedList<PlayerInfo> roomList1 : rooms) {
+				if (roomList1.size() >= limit) {
+					continue;
+				}
+				int standard = roomList1.peek().getLevel();
+				if (standard - 10 <= level && level <= standard + 10) {
+					isFind = true;
+					roomList1.add(new PlayerInfo(level, id));
+					break;
+				}
 
-			player[i] = new PlayerInfo(Integer.parseInt(secInput[0]), secInput[1]);
-			list.add(player[i]);
-		}
-//이 부분 수정 -> for each 문 활용
-		while (count < playerNum) {
-			if (room.size() == 0)
-				curLevel = list.get(next).getLevel();
-
-			if (curLevel - 10 <= list.get(next).getLevel() && list.get(next).getLevel() <= curLevel + 10) {
-
-				room.add(list.get(next));
-
-				count++;
-				list.remove(next);
-			} else {
-				next++;
 			}
+			if (!isFind) {
+				LinkedList<PlayerInfo> roomList = new LinkedList<>();
+				roomList.add(new PlayerInfo(level, id));
+				rooms.add(roomList);
+			}
+
+		}
+		for (LinkedList<PlayerInfo> room : rooms) {
+			bw.write(room.getClass().toString());
+			Collections.sort(room);
 			if (room.size() == limit) {
-				Collections.sort(room);
-				System.out.println("Started!");
-				for (int i = 0; i < room.size(); i++) {
-
-					System.out
-							.println(((PlayerInfo) room.get(i)).getLevel() + " " + ((PlayerInfo) room.get(i)).getId());
-				}
-				room.clear();
-				next = 0;
-				curLevel = 1;
+				bw.write("Started!\n");
+			} else {
+				bw.write("Waiting!\n");
 			}
-			else if (next == list.size()) {
-				System.out.println("Waiting!");
-				for (int i = 0; i < room.size(); i++) {
-
-					System.out
-							.println(((PlayerInfo) room.get(i)).getLevel() + " " + ((PlayerInfo) room.get(i)).getId());
-				}
-				room.clear();
-				next = 0;
-				curLevel = 1;
+			while (!room.isEmpty()) {
+				PlayerInfo info = room.poll();
+				bw.write(info.getLevel() + " " + info.getId() + "\n");
 			}
-
 		}
-	}
+		bw.flush();
+//이 부분 수정 -> for each 문 활용
 
+	}
 }
 
 class PlayerInfo implements Comparable<Object> {
